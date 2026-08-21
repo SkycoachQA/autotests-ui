acd739373186545ba95f8d3186bc551135b16ef3
SkycoachQA <skycoach.qa@gmail.com>
2026-08-21 21:01:15 +0300

    conftest

--- conftest.py
+++ conftest.py
@@ -1,20 +1,4 @@
-import pytest
-
-from pages.login_page import LoginPage
-
-
-@pytest.mark.regression
-@pytest.mark.authorization
-@pytest.mark.parametrize(
-    "email, password",
-    [
-        ("user.name@gmail.com", "password"),
-        ("user.name@gmail.com", "  "),
-        ("  ", "password")
-    ]
+pytest_plugins = (
+    "fixtures.pages",  # Подключаем фикстуры страниц
+    "fixtures.browsers"  # Подключали ранее в предыдущих уроках
 )
-def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str, password: str):
-    login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
-    login_page.fill_login_form(email=email, password=password)
-    login_page.click_login_button()
-    login_page.check_visible_wrong_email_or_password_alert()
